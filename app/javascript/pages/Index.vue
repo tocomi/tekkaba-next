5e829fdd55848999c57fd79ff7cf81e9513ad616
tocomi <tocomi@wish.ocn.ne.jp>
2019-04-22 16:45:55 +0900

    show message

--- app/javascript/pages/Index.vue
+++ app/javascript/pages/Index.vue
@@ -7,7 +7,7 @@
       app
     >
       <v-list dense>
-        <v-list-tile @click="">
+        <v-list-tile @click="hello('dashboad')">
           <v-list-tile-action>
             <v-icon>dashboard</v-icon>
           </v-list-tile-action>
@@ -15,7 +15,7 @@
             <v-list-tile-title>Dashboard</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile @click="">
+        <v-list-tile @click="hello('settings')">
           <v-list-tile-action>
             <v-icon>settings</v-icon>
           </v-list-tile-action>
@@ -68,6 +68,11 @@
     }),
     props: {
       source: String
+    },
+    methods: {
+      hello(message: string) {
+        alert(message)
+      }
     }
   }
 </script>
