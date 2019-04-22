b1f63a697330c7b048cc55dc38f69c075d650131
tocomi <tocomi@wish.ocn.ne.jp>
2019-04-23 00:33:04 +0900

    setting pug

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,9 +3,11 @@ const typescript =  require('./loaders/typescript')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const stylus = require('./loaders/stylus')
+const pug = require('./loaders/pug')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('stylus', stylus)
 environment.loaders.prepend('typescript', typescript)
+environment.loaders.prepend('pug', pug)
 module.exports = environment
