35dba36d0a738948513323187147d1f44c4f0d71
tocomi <tocomi@wish.ocn.ne.jp>
2019-04-22 16:14:31 +0900

    add typescrypt

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+const typescript =  require('./loaders/typescript')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const stylus = require('./loaders/stylus')
@@ -6,4 +7,5 @@ const stylus = require('./loaders/stylus')
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('stylus', stylus)
+environment.loaders.prepend('typescript', typescript)
 module.exports = environment
