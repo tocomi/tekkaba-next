889f814c61c998c613270c1ca1f81cfd663f9dcb
tocomi <tocomi@wish.ocn.ne.jp>
2019-04-23 00:38:32 +0900

    refactor

--- app/javascript/pages/Index.vue
+++ app/javascript/pages/Index.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-  v-app#inspire(dark="")
-    v-navigation-drawer(v-model="drawer" clipped="" fixed="" app="")
-      v-list(dense="")
+  v-app#inspire(dark)
+    v-navigation-drawer(v-model="drawer" clipped fixed app)
+      v-list(dense)
         v-list-tile(@click="hello('dashboad')")
           v-list-tile-action
             v-icon dashboard
@@ -12,22 +12,22 @@
             v-icon settings
           v-list-tile-content
             v-list-tile-title Settings
-    v-toolbar(app="" fixed="" clipped-left="")
+    v-toolbar(app fixed clipped-left)
       v-toolbar-side-icon(@click.stop="drawer = !drawer")
-      v-toolbar-title Application
+      v-toolbar-title tekkaba-next
     v-content
-      v-container(fluid="" fill-height="")
-        v-layout(justify-center="" align-center="")
-          v-flex(shrink="")
-            v-tooltip(right="")
-              v-btn(slot="activator" :href="source" icon="" large="" target="_blank")
-                v-icon(large="") code
+      v-container(fluid fill-height)
+        v-layout(justify-center align-center)
+          v-flex(shrink)
+            v-tooltip(right)
+              v-btn(slot="activator" :href="source" icon large target="_blank")
+                v-icon(large) code
               span Source
-            v-tooltip(right="")
+            v-tooltip(right)
               v-btn(slot="activator" icon="" large="" href="https://codepen.io/johnjleider/pen/qxQWda" target="_blank")
-                v-icon(large="") mdi-codepen
+                v-icon(large) mdi-codepen
               span Codepen
-    v-footer(app="" fixed="")
+    v-footer(app fixed)
       span © 2017
 </template>
 
